30c46acc2ec602309d2eb908fd0610496152314a
yoshimi.satou <youhes@gmail.com>
2025-08-05 16:07:31 +0900

    クラスの命名を修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,17 +1,17 @@
 const mySwiper = new Swiper('.swiper', {
-  loop: true,
-  slidesPerView: 1.24,
-  spaceBetween: 15,
-  centeredSlides: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 3, // 768px以上の場合の表示数
-      slidesPerGroup: 2,
+    loop: true,
+    slidesPerView: 1.24,
+    spaceBetween: 15,
+    centeredSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
     },
-  },
+    breakpoints: {
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 3, // 768px以上の場合の表示数
+            slidesPerGroup: 2,
+        },
+    },
 });
